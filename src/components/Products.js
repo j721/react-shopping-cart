d2daaa5ff916f80763468be4c9ef478b48743c46
j721 <juliadvu@gmail.com>
2020-04-16 14:54:15 -0500

    imported ProductContext and passed in value props

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React,{ useContext } from 'react';
+
+import { ProductContext } from '../contexts/ProductContext';
+
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products =() => {
+	  	// consume data from the context object rather than from props
+		// context hook pattern :)
+		//  - import the context object
+		//  - call the context hook and pass in the context object
+	
+	const {products, addItem} = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
